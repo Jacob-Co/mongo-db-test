272d8d9541a7320f3cd9bc5944202e8a8820b749
Jacobtco <jacobtco@gmail.com>
2020-07-31 18:47:56 +0800

    Added test DELETE /todos/:id

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 
 // mongoose.Promise = global.Promise;
 mongoose.set('useUnifiedTopology', true);
+mongoose.set('useFindAndModify', false);
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/UserApp', {useNewUrlParser: true});
 
 module.exports = {mongoose};
--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -101,3 +101,49 @@ describe('GET /todos/:id', () => {
   });
 
 })
+
+describe('DELETE /todos/:id', () => {
+  it('should remove a todo', (done) => {
+    let hexId = todos[0]._id.toHexString();
+
+    request(app)
+    .delete(`/todos/${hexId}`)
+    .expect(200)
+    .expect(res => {
+      expect(res.body.todo._id).is.equal(hexId)
+    })
+    .end((err, res) => {
+      if (err) {
+        return done(err);
+      }
+
+      Todo.findById(hexId).then(todo => {
+        expect(todo).to.not.exist;
+        done();
+      }).catch(done)
+
+      // Todo.find().then(todos => {
+      //   expect(todos.length).to.equal(1);
+      //   done();
+      // }).catch(done);
+    });
+  });
+
+  it('should return 404 if todo not found', (done) => {
+    let hexId = new ObjectID().toHexString();
+
+    request(app)
+    .delete(`/todos/${hexId}`)
+    .expect(404)
+    .end(done);
+  });
+
+  it('should return 404 if object id is invalid', (done) => {
+    let hexId = '1234';
+
+    request(app)
+    .delete(`/todos/${hexId}`)
+    .expect(404)
+    .end(done);
+  });
+})
