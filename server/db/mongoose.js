bf004e71d6536341ffc7a8d105e9b19241a52592
Jacobtco <jacobtco@gmail.com>
2020-07-31 17:29:50 +0800

    Added new connect path to mLAB

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,6 +2,6 @@ const mongoose = require('mongoose');
 
 // mongoose.Promise = global.Promise;
 mongoose.set('useUnifiedTopology', true);
-mongoose.connect('mongodb://localhost:27017/UserApp', {useNewUrlParser: true});
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/UserApp', {useNewUrlParser: true});
 
 module.exports = {mongoose};
